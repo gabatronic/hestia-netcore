cb2f2f5299f6e68b28d0d01f62c56d5b46de5234
Miguel Angel Gabaldon <gabatronic@hotmail.com>
2023-03-20 19:44:55 +0100

    Remove unneed script solved by single CLI -

--- netcoresock.sh
+++ netcoresock.sh
@@ -4,11 +4,12 @@ domain=$2
 ip=$3
 home=$4
 docroot=$5
-prvdot=$(ps -fu $user | grep dotnet | cut -c 12-17)
+prvdot=$(ps -fu $user | grep "$domain" | cut -c 12-17)
+echo $prvdot
 mkdir "$home/$user/web/$domain/netcoreapp"
 chown -R $user:$user "$home/$user/web/$domain/netcoreapp"
-rm "$home/$user/web/$domain/netcoreapp/app.sock"
+rm $home/$user/web/$domain/netcoreapp/app.sock
 kill -9 $prvdot
-runuser -l $user -c "nohup /usr/bin/dorun $home $user $domain &" > /dev/null
+runuser -l $user -c "cd $home/$user/web/$domain/netcoreapp && nohup /usr/bin/dotnet $home/$user/web/$domain/netcoreapp/app.dll &" > /dev/null
 sleep 5
-chmod 777 "$home/$user/web/$domain/netcoreapp/app.sock"
+chmod 777 $home/$user/web/$domain/netcoreapp/app.sock
